9eb58eb317b607e9ecb1e3e7ce8de1ea4ad02a75
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-02-18 12:31:05 -0700

    initalized fib

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
 function fib(n) {
-    return n;
+    var array = [0]; 
+    findFib(n, array); 
+    return array; 
 }
+
+function findFib(n, array) {
+    if (n < 0) { 
+        return 0; 
+    }
+
+    if (n === 0) { 
+        return 0; 
+    }
+
+    if (n === 1) {
+        array[1] = 1; 
+        return 1; 
+    }
+
+    array[n] = (findFib(n - 1, array)) + findFib(n - 2, array);  
+    return array[n]; 
+}
+
+
+console.log(fib(7)); 
